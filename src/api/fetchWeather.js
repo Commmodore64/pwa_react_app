bdd442c006c08f6f9da65dd78395ec847793149c
Cesar Marines <cesarml2002@hotmail.com>
2022-11-20 18:53:18 -0700

    Final version PWA Weather App

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather'; //url from the weather page
-const API KEY = 'f33a484cf794d08d0148764789aaba32'; //API from the weather page for use
+const API_KEY = 'f33a484cf794d08d0148764789aaba32'; //API from the weather page for use
 
-const fetchWeather = async (query) => {
-    const response = await axios.get(URL, {
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
         params:{
             q: query,
             units: 'metric',
-            APPID: 'API_KEY'.
+            APPID: API_KEY,
         }
     });
+
+    return data;
 }
